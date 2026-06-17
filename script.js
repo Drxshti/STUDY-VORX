a76357ca1d8094a17a11e675576ee7c46a4fd371
Drishti Galani <95745533+Drxshti@users.noreply.github.com>
2026-06-17 15:12:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -184,7 +184,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 try {
                     // YOUR ACTIVE API KEY
-                    const GEMINI_API_KEY = 'AQ.Ab8RN6LzKNQOto3d4YuIfAfrj5F_N2QResoCql2AMsKDOSytVQ';
+                    const GEMINI_API_KEY = 
                     
                     // FIXED BUG A: Updated endpoint url to generativelanguage
                     const GEMINI_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`;
